930e7c14d3a017a59719613e563e1bee2fc7b567
choco-bear <kdy030529@gmail.com>
2025-10-27 04:43:20 +0900

    Defined Hom (co|contra)variant functors

---
+++ Theory/Functor/Hom.v
@@ -0,0 +1,38 @@
+Require Import Category.Lib.
+Require Import Category.Theory.Category.
+Require Import Category.Theory.Functor.
+Require Import Category.Instance.Sets.
+Require Import Category.Construction.Opposite.
+
+Generalizable All Variables.
+
+Section Hom.
+  Context {C : Category}.
+
+  Program Definition HomFunctor (x : C) : C ⟶ Sets :=
+    {| fobj := λ y, {| carrier := hom x y
+                     ; is_setoid := homset x y
+                    |}
+     ; fmap := λ _ _ f, {| morphism := λ g, f ∘ g |}
+    |}.
+
+  Program Definition HomFunctor_op (y : C) : C^op ⟶ Sets :=
+    {| fobj := λ x, {| carrier := @hom C x y
+                     ; is_setoid := @homset C x y
+                    |}
+     ; fmap := λ _ _ f, {| morphism := λ g, g ∘ f |}
+    |}.
+End Hom.
+
+Arguments HomFunctor {_%_category_scope} {_%_object_scope}.
+Arguments HomFunctor_op {_%_category_scope} {_%_object_scope}.
+
+Notation "'Hom(' x ',-)'" := (HomFunctor x%object)
+  (at level 0) : functor_scope.
+Notation "'Hom[' C '](' x ',-)'" := (@HomFunctor C%category x%object)
+  (at level 0) : functor_scope.
+
+Notation "'Hom(-,' y ')'" := (HomFunctor_op y%object)
+  (at level 0) : functor_scope.
+Notation "'Hom[' C '](-,' y ')'" := (@HomFunctor_op C%category y%object)
+  (at level 0) : functor_scope.

e39f5a518c8b461572c48b27163791585c33c09a
choco-bear <kdy030529@gmail.com>
2025-11-07 22:42:42 +0900

    Write a sketch of the proof of correspondence between `C` and `1 ⟶ C`

--- Instance/One.v
+++ Instance/One.v
@@ -1,6 +1,10 @@
 Require Import Category.Lib.
 Require Import Category.Theory.Category.
+Require Import Category.Theory.Isomorphism.
 Require Import Category.Theory.Functor.
+Require Import Category.Theory.Functor.Setoid.
+Require Import Category.Theory.Natural.
+Require Import Category.Construction.Fun.
 
 Generalizable All Variables.
 
@@ -26,3 +30,51 @@ Program Instance Erase `(C : Category) : C ⟶ 1 :=
   {| fobj := fun _ => ttt
    ; fmap := fun _ _ _ => id
   |}.
+
+Section Lemmas.
+  Context {C : Category}.
+
+  (** Any two functors to [1] are equal. *)
+  Lemma Functor_to_1_unique (F G : C ⟶ 1) : F ≡ G.
+  Proof.
+    srapply Component_Is_Iso_NatIso; construct; cat.
+  Qed.
+
+  Lemma name (F : 1 ⟶ C) : fmap[F] ttt ≡ id[F ttt].
+  Proof.
+    (* idk why but [autorewrite with categories] fall in infinite loop *)
+    admit.
+  Admitted.
+End Lemmas.
+
+Section Correspondence.
+  Context {C : Category}.
+
+  (** There is a one-to-one correspondence between objects of [C] and functors
+    * from [1] to [C]. *)
+  Program Definition Wrap (x : C) : 1 ⟶ C :=
+    {| fobj := λ _, x
+     ; fmap := λ _ _ _, id[x]
+
+     ; fmap_respects := λ _ y f g fg, reflexivity id[x]
+     ; fmap_id := λ _, reflexivity id[x]
+     ; fmap_comp := λ _ _ _ f g, symmetry (id_right id[x])
+    |}.
+  
+  Definition unwrap (F : 1 ⟶ C) : C := F ttt.
+
+  Lemma Wrap_unwrap (F : 1 ⟶ C) : Wrap (unwrap F) ≡ F.
+  Proof.
+    unfold unwrap. srapply Component_Is_Iso_NatIso.
+    { (* Definition of η *)
+      construct.
+      - destruct x. exact (id[F ttt]).
+      - simpl. destruct x, y, f.
+        now rewrite name.
+    } simpl.
+    intros []. construct; [exact id|..]; cat.
+  Qed.
+
+  Lemma unwrap_Wrap (x : C) : unwrap (Wrap x) = x.
+  Proof. reflexivity. Qed.
+End Correspondence.

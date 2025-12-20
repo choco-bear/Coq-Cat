841b20e09dbcaa3ee966df1cf490e510536f54ad
choco-bear <kdy030529@gmail.com>
2025-12-20 22:59:15 +0900

    Every cyclic group is cycic

--- Theory/Group/Cyclic.v
+++ Theory/Group/Cyclic.v
@@ -22,3 +22,26 @@ Next Obligation. replace (_ - _)%Z with 0%Z by nia. apply divide_0_r. Qed.
 Next Obligation. replace (_ - _)%Z with 0%Z by nia. apply divide_0_r. Qed.
 Next Obligation. replace (_ - _)%Z with 0%Z by nia. apply divide_0_r. Qed.
 Next Obligation. replace (_ - _)%Z with 0%Z by nia. apply divide_0_r. Qed.
+
+#[export]
+Program Instance cyclic_group_is_cyclic n
+  : IsCyclic (mk_cyclic_group n) := {| generator := 1 |}%Z.
+Next Obligation.
+  exists g. assert ((1%Z : mk_cyclic_group n) ^ g = g)%group; cycle 1.
+  { rewrite H, Z.sub_diag. apply divide_0_r. }
+  destruct g; ss.
+  - replace (Z.pos p) with (Z.pos p + 0)%Z by lia.
+    remember 0%Z as m; clear Heqm; revert m.
+    induction p; intro; try done.
+    + replace (p~1)%positive with (1 + p + p)%positive by lia.
+      rewrite !Pos.iter_add, !IHp, !Pos2Z.inj_add; unfold Pos.iter; lia.
+    + replace (p~0)%positive with (p + p)%positive by lia.
+      rewrite Pos.iter_add, !IHp, !Pos2Z.inj_add; lia.
+  - replace (Z.neg p) with (Z.neg p + 0)%Z by lia.
+    remember 0%Z as m; clear Heqm; revert m.
+    induction p; intro; try done.
+    + replace (p~1)%positive with (1 + p + p)%positive by lia.
+      rewrite !Pos.iter_add, !IHp, <-!Pos2Z.add_neg_neg; unfold Pos.iter; lia.
+    + replace (p~0)%positive with (p + p)%positive by lia.
+      rewrite Pos.iter_add, !IHp, <-!Pos2Z.add_neg_neg; lia.
+Qed.

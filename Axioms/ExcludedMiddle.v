f41a02aea66d98b262045282fbb2e64b4e0ba984
choco-bear <kdy030529@gmail.com>
2026-01-02 09:23:06 +0900

    Renamed the file

---
+++ Axioms/ExcludedMiddle.v
@@ -0,0 +1,24 @@
+Require Import Category.Lib.Tactics.
+
+Generalizable All Variables.
+Set Primitive Projections.
+Set Universe Polymorphism.
+Unset Transparent Obligations.
+
+Axiom ExcludedMiddle : Type.
+Existing Class ExcludedMiddle.
+
+Axiom LEM : ∀ `{ExcludedMiddle} (P : Type), P ∨ ¬ P.
+#[export] Hint Resolve LEM : core.
+
+Tactic Notation "classic" uconstr(P) := destruct (LEM P).
+Tactic Notation "classic" uconstr(P) "as" ident(H) := destruct (LEM P) as [H|H].
+Tactic Notation "classic" :=
+  repeat match goal with
+  | |- context[LEM ?x] => destruct (LEM x); cat
+  end.
+
+
+Global Instance ExcludedMiddle_Decidable
+  `{ExcludedMiddle} `(SET : Setoid A) : Decidable SET.
+Proof. construct; auto. Qed.

10b921db0063e01a030ff3dfae39c8704ad9a490
choco-bear <kdy030529@gmail.com>
2026-01-11 09:42:10 +0900

    Defined the category `Fin` of the finite setoids

---
+++ Instance/Fin.v
@@ -0,0 +1,39 @@
+Require Import Category.Lib.
+Require Import Category.Axioms.ExcludedMiddle.
+Require Import Category.Theory.Category.
+Require Import Category.Theory.Isomorphism.
+Require Import Category.Instance.Sets.
+
+Generalizable All Variables.
+Set Universe Polymorphism.
+
+(** A finite object is a setoid object equipped with a finiteness proof. *)
+Record FiniteObject : Type :=
+  { carrier :> SetoidObject
+  ; is_finite :> Finite (is_setoid carrier)
+  }.
+#[export] Existing Instance is_finite.
+
+Definition of_finite `{SET : Setoid A} (FIN : Finite SET) : FiniteObject :=
+  {|  carrier := of_setoid SET
+    ; is_finite := FIN
+  |}.
+#[export] Coercion of_finite : Finite >-> FiniteObject.
+
+(** The category of finite types *)
+Program Definition Fin : Category :=
+  {|  obj := FiniteObject
+    ; hom := @SetoidMorphism
+    ; homset := @SetoidMorphism_Setoid
+
+    ; id := @SetoidMorphism_id
+    ; compose := @SetoidMorphism_compose
+    ; compose_respects := @SetoidMorphism_compose_respects
+  |}.
+
+#[export]
+Program Instance isomorphism_preserves_cardinality `{ExcludedMiddle}
+  : Proper (Isomorphism ==> eq) (λ X : Fin, cardinality X).
+Next Obligation.
+  (* TODO *)
+Admitted.

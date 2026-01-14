db8a929c8045af0100f747dd34e9c55a31ef7258
choco-bear <kdy030529@gmail.com>
2026-01-14 20:14:42 +0900

    Modified the definition of Hom functors by defining Hom bifunctor

--- Theory/Functor/Hom.v
+++ Theory/Functor/Hom.v
@@ -3,6 +3,7 @@ Require Import Category.Theory.Category.
 Require Import Category.Theory.Functor.
 Require Import Category.Instance.Sets.
 Require Import Category.Construction.Opposite.
+Require Import Category.Construction.Product.
 
 Generalizable All Variables.
 
@@ -10,23 +11,33 @@ Generalizable All Variables.
 Section Hom.
   Context {C : Category}.
 
-  Program Definition HomFunctor (x : C) : C ⟶ Sets :=
-    {| fobj := λ y, {| carrier := hom x y
-                     ; is_setoid := homset x y
-                    |}
-     ; fmap := λ _ _ f, {| morphism := λ g, f ∘ g |}
+  Program Definition HomBiFunctor : C^op × C ⟶ Sets :=
+    {|  fobj :=
+          λ p : obj[C^op × C],
+            match p return obj[Sets] with
+            | (x,y) => {| carrier := hom[C] x y 
+                        ; is_setoid := @homset C x y
+                       |}
+            end
+      ; fmap := λ`(x1,y1) : obj[C^op × C],
+                λ`(x2,y2) : obj[C^op × C],
+                λ`(f,g) : (x1,y1) ~{C^op × C}~> (x2,y2),
+                  {| morphism := λ X, g ∘ X ∘ f |}
     |}.
+  
+  Program Definition HomFunctor (x : C) : C ⟶ Sets :=
+    HomBiFunctor ◯ (Const (x : C^op) × Id[C]).
 
   Program Definition HomFunctor_op (y : C) : C^op ⟶ Sets :=
-    {| fobj := λ x, {| carrier := @hom C x y
-                     ; is_setoid := @homset C x y
-                    |}
-     ; fmap := λ _ _ f, {| morphism := λ g, g ∘ f |}
-    |}.
+    HomBiFunctor ◯ (Id[C^op] × Const y).
 End Hom.
 
 Arguments HomFunctor {_%_category_scope} {_%_object_scope}.
 Arguments HomFunctor_op {_%_category_scope} {_%_object_scope}.
+
+Notation "'Hom(-,-)'" := (@HomBiFunctor _) (at level 9) : functor_scope.
+Notation "'Hom[' C '](-,-)'" := (@HomBiFunctor C)
+  (at level 9, only parsing) : functor_scope.
 
 Notation "'Hom(' x ',-)'" := (@HomFunctor _ x%object)
   (at level 9, format "Hom( x ,-)") : functor_scope.

2f2df80e17f7bc1b28a3b9990702381bf47b70e9
choco-bear <kdy030529@gmail.com>
2026-02-14 02:49:05 +0900

    proved the extensionality for categories, and involutivity of opposite category

--- theories/category/Category.v
+++ theories/category/Category.v
@@ -1,5 +1,4 @@
-Require Import Program Axioms sflib.
-From stdpp Require Import ssreflect.
+From stdpp Require Import prelude.
 
 Class Category (Obj : Type) := mk_Category {
   Arrow : Obj → Obj → Type;
@@ -18,10 +17,9 @@ Class Category (Obj : Type) := mk_Category {
 }.
 
 Local Definition _obj [T : Type] : Category T → Type := λ _, T.
-Coercion _obj : Category >-> Sortclass.
 
-Local Definition _dom `{!Category Obj} `(Arrow x y) : Obj := x.
-Local Definition _cod `{!Category Obj} `(Arrow x y) : Obj := y.
+Local Definition _dom `{Category Obj} `(Arrow x y) : Obj := x.
+Local Definition _cod `{Category Obj} `(Arrow x y) : Obj := y.
 
 Declare Scope coqcat_scope.
 Declare Scope category_scope.
@@ -42,11 +40,12 @@ Arguments _obj [Obj%_type_scope] C%_category_scope : rename.
 Arguments Arrow {Obj%_type_scope C%_category_scope} (x y)%_object_scope : rename.
 Arguments _dom {Obj%_type_scope C%_category_scope x%_object_scope y%_object_scope} f%_morphism_scope : rename.
 Arguments _cod {Obj%_type_scope C%_category_scope x%_object_scope y%_object_scope} f%_morphism_scope : rename.
-Arguments cat_id {Obj%_type_scope C%_category_scope x%_object_scope} : rename, simpl never.
+Arguments comp_proper {_%_type_scope _%_category_scope} {_ _ _}%_object_scope (f f)%_morphism_scope _ (g g)%_morphism_scope _ : rename.
 Arguments comp {Obj%_type_scope C%_category_scope} {x y z}%_object_scope (f g)%_morphism_scope : rename, simpl never.
+Arguments cat_id {Obj%_type_scope C%_category_scope x%_object_scope} : rename, simpl never.
 
-Notation "'obj[' C ']'" := (_obj C%category) (at level 7, only parsing) : type_scope.
-Notation "'hom[' C ']'" := (@Arrow _ C%category) (at level 7, only parsing) : type_scope.
+Notation "'obj[' C ']'" := (_obj C%category) (at level 9, no associativity, format "obj[ C ]") : type_scope.
+Notation "'hom[' C ']'" := (@Arrow _ C%category) (at level 9, no associativity, format "hom[ C ]") : type_scope.
 Notation "'dom' f" := (_dom f%morphism) (at level 8, right associativity) : object_scope.
 Notation "'cod' f" := (_cod f%morphism) (at level 8, right associativity) : object_scope.
 
@@ -56,17 +55,17 @@ Notation "x '<~' y" := (y ~> x) (at level 90, only parsing) : type_scope.
 Notation "x '<~{' C '}~' y" := (y ~{C}~> x) (at level 90, only parsing) : type_scope.
 
 Notation "'id'" := cat_id (at level 0, only parsing) : morphism_scope.
-Notation "'id[' x ']'" := (@cat_id _ _ x%object) (at level 7, format "id[ x ]") : morphism_scope.
-Notation "'id{' C '}'" := (@cat_id _ C%category _) (at level 7, only parsing) : morphism_scope.
-Notation "'id{' C '}[' x ']'" := (@cat_id _ C%category x%object) (at level 7, only parsing) : morphism_scope.
+Notation "'id[' x ']'" := (@cat_id _ _ x%object) (at level 9, no associativity, format "id[ x ]") : morphism_scope.
+Notation "'id{' C '}'" := (@cat_id _ C%category _) (at level 9, no associativity, only parsing) : morphism_scope.
+Notation "'id{' C '}[' x ']'" := (@cat_id _ C%category x%object) (at level 9, no associativity, only parsing) : morphism_scope.
 
 Notation "f ∘ g" := (comp f%morphism g%morphism) : morphism_scope.
 Notation "f '∘[' C ']' g" := (@comp _ C%category _ _ _ f%object g%object) (at level 40, only parsing) : morphism_scope.
 
 Notation "f '≡[' C ']' g" := (f%morphism ≡[hom[C%category] _ _] g%morphism)
-  (at level 70, no associativity, only parsing) : coqcat_scope.
+  (at level 60, no associativity, only parsing) : coqcat_scope.
 
-Definition Opposite [Obj : Type] (C : Category Obj) : Category Obj :=
+Definition Opposite `(C : Category Obj) : Category Obj :=
   {|
     Arrow := λ x y, Arrow y x;
     Arrow_equiv := λ x y, Arrow_equiv y x;
@@ -79,3 +78,7 @@ Definition Opposite [Obj : Type] (C : Category Obj) : Category Obj :=
     cat_id_left := λ x y f, @cat_id_right Obj C y x f;
     cat_id_right := λ x y f, @cat_id_left Obj C y x f
   |}.
+
+Notation "C 'ᵒᵖ'" := (Opposite C%category) (at level 7, left associativity, format "C ᵒᵖ") : category_scope.
+Notation "'(ᵒᵖ)'" := (@Opposite _) (only parsing) : coqcat_scope.
+Notation "'(ᵒᵖ)@{' Obj '}'" := (@Opposite Obj%type) (at level 9, no associativity, only parsing) : coqcat_scope.
--- theories/common/Common.v
+++ theories/common/Common.v
@@ -2,6 +2,6 @@ Require Export Program Axioms sflib.
 Require Export Permutation Orders String HexString ZArith.
 Require Import List.
 Export ListNotations.
-Require Export Category.
+Require Export Category CategoryFacts.
 
 From stdpp Require Export ssreflect.

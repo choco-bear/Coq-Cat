b797f041eb3d9588733fd173f456ef0ee5861fdb
choco-bear <kdy030529@gmail.com>
2026-02-15 15:56:35 +0900

    defined the powerset functor

--- theories/categories/Sets.v
+++ theories/categories/Sets.v
@@ -12,4 +12,22 @@ Program Instance Sets : Category Type :=
 
 Lemma Sets_id_unfold X x : id{Sets}[X]%morphism x = x.
 Proof. rewrite /cat_id //. Qed.
-#[export] Hint Rewrite @Sets_id_unfold : normalize.
+Lemma Sets_comp_unfold [X Y Z : Type] (f : Y ~> Z) (g : X ~> Y) x : (f ∘ g)%morphism x = f (g x).
+Proof. rewrite /comp //. Qed.
+#[export] Hint Rewrite @Sets_id_unfold @Sets_comp_unfold : normalize.
+
+Program Definition Powerset : Sets ⟶ Sets :=
+  {|
+    fobj := λ X, X → Prop;
+    fmap := λ X Y f P y, ∃ x, P x ∧ y = f x;
+  |}.
+Next Obligation.
+  rename x into X.
+  apply func_ext=> P. normalize. apply pred_ext=> x.
+  split=> [[y] [/[swap] ->]|]; normalize; eauto.
+Qed.
+Next Obligation.
+  rename x into X. rename y into Y. rename z into Z.
+  apply func_ext=> P. normalize. apply pred_ext=> z.
+  split=> [[x] [/[swap] ->]|[y] [[x] /[swap] ->] [/[swap] ->]]; normalize; eauto.
+Qed.

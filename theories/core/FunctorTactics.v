0ba00029751b4e665afe98d5b52e496f70b9d70b
choco-bear <kdy030529@gmail.com>
2026-02-15 22:51:06 +0900

    feat: enhance functor tactics with additional rewrites and scope management

--- theories/core/FunctorTactics.v
+++ theories/core/FunctorTactics.v
@@ -1,7 +1,5 @@
-Require Import CommonTactics Category.
+Require Import CommonTactics CommonFacts Category.
 Require Export Functor.
-
-Local Open Scope functor_scope.
 
 Create HintDb functor_app discriminated.
 Create HintDb functor_laws discriminated.
@@ -9,6 +7,12 @@ Create HintDb fmap_norm discriminated.
 
 Global Hint Rewrite @Id_fobj @Id_fmap @Const_fobj @Const_fmap @Comp_fobj @Comp_fmap : functor_app.
 Global Hint Rewrite @fmap_id @fmap_comp : fmap_norm.
+Global Hint Rewrite @inv_functor_left @inv_functor_right : functor_laws.
+
+Ltac functor_norm := autorewrite with functor_laws; autorewrite with functor_app; autorewrite with fmap_norm.
+Tactic Notation "functor_norm" "in" hyp(H) := autorewrite with functor_laws in H; autorewrite with functor_app in H; autorewrite with fmap_norm in H.
+Tactic Notation "functor_norm" "in" "*" "|-" := repeat_on_hyps (fun H => functor_norm in H).
+Tactic Notation "functor_norm" "in" "*" := autorewrite with functor_laws in *; autorewrite with functor_app in *; autorewrite with fmap_norm in *.
 
 Lemma functor_ext `{C : Category ObjC} `{D : Category ObjD} (F G : C ⟶ D)
   : fobj F = fobj G
@@ -22,22 +26,22 @@ Qed.
 
 Section FunctorLaws.
   Context `{C : Category ObjC} `{D : Category ObjD}.
+  Local Open Scope functor_scope.
 
   Lemma functor_id_left (F : C ⟶ D) : Id ∘ F = F.
-  Proof. apply functor_ext; first apply func_ext; i; autorewrite with functor_app=> //. Qed.
+  Proof. apply functor_ext; first apply func_ext; i; functor_norm=> //. Qed.
 
   Lemma functor_id_right (F : C ⟶ D) : F ∘ Id = F.
-  Proof. apply functor_ext; first apply func_ext; i; autorewrite with functor_app=> //. Qed.
+  Proof. apply functor_ext; first apply func_ext; i; functor_norm=> //. Qed.
 
   Lemma functor_comp_assoc (F : C ⟶ D) `(G : (B : Category ObjB) ⟶ C) `(H : (A : Category ObjA) ⟶ B)
     : F ∘ (G ∘ H) = F ∘ G ∘ H.
-  Proof. apply functor_ext; first apply func_ext; i; autorewrite with functor_app=> //. Qed.
+  Proof. apply functor_ext; first apply func_ext; i; functor_norm=> //. Qed.
 End FunctorLaws.
 Global Hint Rewrite @functor_id_left @functor_id_right @functor_comp_assoc : functor_laws.
 
+Section Inverses.
+  Context `{C : Category ObjC} `{D : Category ObjD} (F : C ⟶ D) `{!IsFunctorIso F}.
 
-(** Tactics **)
-Ltac functor_norm := autorewrite with functor_laws; autorewrite with functor_app; autorewrite with fmap_norm.
-Tactic Notation "functor_norm" "in" hyp(H) := autorewrite with functor_laws in H; autorewrite with functor_app in H; autorewrite with fmap_norm in H.
-Tactic Notation "functor_norm" "in" "*" "|-" := repeat_on_hyps (fun H => functor_norm in H).
-Tactic Notation "functor_norm" "in" "*" := autorewrite with functor_laws in *; autorewrite with functor_app in *; autorewrite with fmap_norm in *.
+  (* TODO : simplification lemmas for inverse functor *)
+End Inverses.

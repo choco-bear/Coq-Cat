6357be80e09cbfb05d580bb8b1b2606816826e06
choco-bear <kdy030529@gmail.com>
2026-02-16 04:55:14 +0900

    fix: update fapply tactic to use uconstr for better type handling

--- theories/common/CommonTactics.v
+++ theories/common/CommonTactics.v
@@ -31,5 +31,5 @@ Global Obligation Tactic := program_simpl; common_simpl.
 Lemma fapply [A B : Type] (f : A → B) x y : x = y → f x = f y.
 Proof. i; rewrite H //. Qed.
 
-Tactic Notation "fapply" constr(f) "in" hyp(H) := eapply (fapply f) in H.
-Tactic Notation "fapply" constr(f) "in" hyp(H) "as" ident(name) := eapply (fapply f) in H as name. 
+Tactic Notation "fapply" uconstr(f) "in" hyp(H) := eapply (fapply f) in H.
+Tactic Notation "fapply" uconstr(f) "in" hyp(H) "as" ident(name) := eapply (fapply f) in H as name. 

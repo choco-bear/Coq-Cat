3554dbd099f6ab02926e26a8c7081653494721bd
choco-bear <kdy030529@gmail.com>
2026-02-15 21:59:16 +0900

    tac: enhance functor_norm

--- theories/core/FunctorTactics.v
+++ theories/core/FunctorTactics.v
@@ -5,8 +5,10 @@ Local Open Scope functor_scope.
 
 Create HintDb functor_app discriminated.
 Create HintDb functor_laws discriminated.
+Create HintDb fmap_norm discriminated.
 
 Global Hint Rewrite @Id_fobj @Id_fmap @Const_fobj @Const_fmap @Comp_fobj @Comp_fmap : functor_app.
+Global Hint Rewrite @fmap_id @fmap_comp : fmap_norm.
 
 Lemma functor_ext `{C : Category ObjC} `{D : Category ObjD} (F G : C ⟶ D)
   : fobj F = fobj G
@@ -35,7 +37,7 @@ Global Hint Rewrite @functor_id_left @functor_id_right @functor_comp_assoc : fun
 
 
 (** Tactics **)
-Ltac functor_norm := autorewrite with functor_laws; autorewrite with functor_app.
-Tactic Notation "functor_norm" "in" hyp(H) := autorewrite with functor_laws in H; autorewrite with functor_app in H.
+Ltac functor_norm := autorewrite with functor_laws; autorewrite with functor_app; autorewrite with fmap_norm.
+Tactic Notation "functor_norm" "in" hyp(H) := autorewrite with functor_laws in H; autorewrite with functor_app in H; autorewrite with fmap_norm in H.
 Tactic Notation "functor_norm" "in" "*" "|-" := repeat_on_hyps (fun H => functor_norm in H).
-Tactic Notation "functor_norm" "in" "*" := autorewrite with functor_laws in *; autorewrite with functor_app in *.
+Tactic Notation "functor_norm" "in" "*" := autorewrite with functor_laws in *; autorewrite with functor_app in *; autorewrite with fmap_norm in *.

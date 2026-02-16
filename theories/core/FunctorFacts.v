2dde3ca54fdf428438b20ac2658d90ae33bb0cd8
choco-bear <kdy030529@gmail.com>
2026-02-16 11:50:34 +0900

    refactor: correct import statement and add proof irrelevance for functor isomorphisms

--- theories/core/FunctorFacts.v
+++ theories/core/FunctorFacts.v
@@ -1,4 +1,4 @@
-Require Import Common CommonTactics Category Functor FunctorTactics.
+Require Import CommonFacts CommonTactics Category Functor FunctorTactics.
 
 Local Open Scope functor_scope.
 
@@ -17,3 +17,12 @@ Section InverseUnique.
   Lemma functor_inv_right_unique : F ∘ G = Id → G = F⁻¹.
   Proof. i; transitivity (F⁻¹ ∘ F ∘ G); last rewrite -functor_comp_assoc H; functor_norm //. Qed.
 End InverseUnique.
+
+Global Instance iso_functor_proof_irrel
+  `{C : Category ObjC} `{D : Category ObjD} (F : C ⟶ D) : ProofIrrel (IsFunctorIso F).
+Proof.
+  ii. depdes x. cut (inverse_functor = F⁻¹); try by apply functor_inv_left_unique.
+  intros ->. depdes y. replace inv_functor_left0 with inv_functor_left by apply proof_irr.
+  replace inv_functor_right0 with inv_functor_right by apply proof_irr.
+  rewrite /Functor.inverse_functor //.
+Qed.

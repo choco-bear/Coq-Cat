70423d6e16d7673a25fc83ff436dca3824a661c6
choco-bear <kdy030529@gmail.com>
2026-02-18 12:50:03 +0900

    feat: add Mon and Grp modules with category definitions and forgetful functors

---
+++ theories/categories/Concrete.v
@@ -0,0 +1,50 @@
+From mathcomp Require Import ssreflect all_algebra.
+Require Import Common Sets.
+
+Module Mon.
+  Structure Object := {
+    ObjM : Type;
+    #[export] monoid :> Category ObjM;
+    #[export] is_group :: IsMonoid monoid;
+  }.
+
+  Local Notation MonArrow := (λ M M' : Object, M ⟶ M').
+
+  Program Definition Mon : Category Object :=
+    {|
+      Arrow   := MonArrow;
+      comp    := λ M1 M2 M3 ϕ ψ, ϕ ∘ ψ;
+      cat_id  := λ M, Id[M];
+    |}%functor.
+  Solve Obligations with functor_solver.
+
+  Program Definition Forgetful : Mon ⟶ Sets :=
+    {|
+      fobj := λ M, obj[M];
+      fmap := λ M1 M2 ϕ, fobj ϕ;
+    |}.
+End Mon.
+
+Module Grp.
+  Structure Object := {
+    ObjG : Type;
+    #[export] group :> Category ObjG;
+    #[export] is_group :: IsGroup group;
+  }.
+
+  Local Notation GrpArrow := (λ G G' : Object, G ⟶ G').
+  
+  Program Definition Grp : Category Object :=
+    {|
+      Arrow   := GrpArrow;
+      comp    := λ G H K ϕ ψ, ϕ ∘ ψ;
+      cat_id  := λ G, Id[G];
+    |}%functor.
+  Solve Obligations with functor_solver.
+
+  Program Definition Forgetful : Grp ⟶ Sets :=
+    {|
+      fobj := λ G, obj[G];
+      fmap := λ G H ϕ, fobj ϕ;
+    |}.
+End Grp.

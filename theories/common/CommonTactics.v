97b62c1c1dc67f3837c0337cab847b224e0c35f4
choco-bear <kdy030529@gmail.com>
2026-02-18 15:31:47 +0900

    refactor: enhance Unique class and related tactics for improved usability

--- theories/common/CommonTactics.v
+++ theories/common/CommonTactics.v
@@ -9,29 +9,39 @@ Create HintDb coqcat discriminated.
 
 Global Hint Rewrite @compose_id_right @compose_id_left : normalize.
 
-Class Unique (A : Type) := {
-  #[export] unique_inhabited :: Inhabited A;
-  #[export] unique_proof_irrel :: ProofIrrel A;
-}.
-
-Lemma unique_collapse (A : Type) `{!Unique A} : ∀ a : A, a = inhabitant.
-Proof. i. apply proof_irrel. Qed.
-Hint Rewrite @unique_collapse : normalize.
-
-Global Program Instance unit_unique : Unique ().
-Global Program Instance true_unique : Unique True.
-
 Ltac common_normalize := autorewrite with normalize.
 Tactic Notation "common_normalize" "in" hyp(H) := autorewrite with normalize in H.
 Tactic Notation "common_normalize" "in" "*" "|-" := repeat_on_hyps (fun H => common_normalize in H).
 Tactic Notation "common_normalize" "in" "*" := autorewrite with normalize in *.
 
+Class Unique (A : Type) := {
+  #[export] unique_inhabited :: Inhabited A;
+  #[export] unique_proof_irrel :: ProofIrrel A;
+}.
+
+Lemma unique_collapse [A B : Type] (f : A → B) (x : A) `{!Unique B} : f x = inhabitant.
+Proof. apply proof_irrel. Qed.
+
+Global Hint Rewrite @unique_collapse : normalize.
+
 Ltac cat := eauto with coqcat.
+
+Ltac simpl_unique :=
+  hrepeat do 1 match goal with
+  | [x : ?A |- _] =>
+      let A_is_unique := fresh A "_is_unique" in
+      progress tryif (
+        assert (A_is_unique : Unique A) by apply _
+      ) then (
+        depdes x; clear A_is_unique
+      ) else idtac
+  end; hrepeat do 1 progress rewrite @unique_collapse in *.
 
 Ltac common_simpl :=
   ii; ss; subst;
   try apply _;
   try solve_proper;
+  simpl_unique;
   tryif (
     solve [ common_normalize in *; try by cat]
   ) then idtac else (
@@ -39,6 +49,9 @@ Ltac common_simpl :=
   ).
 
 Global Obligation Tactic := program_simpl; common_simpl.
+
+Global Program Instance unit_unique : Unique ().
+Global Program Instance true_unique : Unique True.
 
 Lemma fapply [A B : Type] (f : A → B) [x y : A] : x = y → f x = f y.
 Proof. i; rewrite H //. Qed.

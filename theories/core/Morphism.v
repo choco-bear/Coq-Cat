66e55fde9ff6f83e45b501cd89ab32063d97e7a1
choco-bear <kdy030529@gmail.com>
2026-02-18 12:48:40 +0900

    feat: add IsGroup class with references to IsMonoid and IsGroupoid

--- theories/core/Morphism.v
+++ theories/core/Morphism.v
@@ -116,3 +116,8 @@ Global Hint Rewrite @idempotent @retr_left_inv @sect_right_inv : normalize.
 Class IsGroupoid `(C : Category Obj) := {
   #[export] is_groupoid {x y} (f : x ~> y) :: IsIsomorphism f
 }.
+
+Class IsGroup `(G : Category Obj) := {
+  #[export] group_is_monoid :: IsMonoid G;
+  #[export] group_is_groupoid :: IsGroupoid G;
+}.

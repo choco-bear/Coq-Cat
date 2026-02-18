78a1f9f33735e3bb7fc3bf4643664ab59d95a0b8
choco-bear <kdy030529@gmail.com>
2026-02-18 14:33:15 +0900

    refactor: move coercion registration out of module

--- theories/categories/Concrete.v
+++ theories/categories/Concrete.v
@@ -4,54 +4,56 @@ Require Import Common Sets.
 Module Mon.
   Structure Object := {
     obj : Type;
-    #[export] monoid :> Category obj;
+    monoid : Category obj;
     #[export] is_monoid :: IsMonoid monoid;
   }.
 
-  Local Notation MonArrow := (λ M M' : Object, M ⟶ M').
+  Local Notation MonArrow := (λ M M' : Object, monoid M ⟶ monoid M').
 
   Program Definition Mon : Category Object :=
     {|
       Arrow   := MonArrow;
       comp    := λ M1 M2 M3 ϕ ψ, ϕ ∘ ψ;
-      cat_id  := λ M, Id[M];
+      cat_id  := λ M, Id[monoid M];
     |}%functor.
   Solve Obligations with functor_solver.
 
   Program Definition Forgetful : Mon ⟶ Sets :=
     {|
-      fobj := λ M, obj[M];
+      fobj := λ M, obj[monoid M];
       fmap := λ M1 M2 ϕ, fobj ϕ;
     |}.
 End Mon.
+Coercion Mon.monoid : Mon.Object >-> Category.
 
 Module Grp.
   Structure Object := {
     obj : Type;
-    #[export] group :> Category obj;
+    group : Category obj;
     #[export] is_group :: IsGroup group;
   }.
 
-  Local Notation GrpArrow := (λ G G' : Object, G ⟶ G').
+  Local Notation GrpArrow := (λ G G' : Object, group G ⟶ group G').
   
   Program Definition Grp : Category Object :=
     {|
       Arrow   := GrpArrow;
       comp    := λ G H K ϕ ψ, ϕ ∘ ψ;
-      cat_id  := λ G, Id[G];
+      cat_id  := λ G, Id[group G];
     |}%functor.
   Solve Obligations with functor_solver.
 
   Program Definition Forgetful : Grp ⟶ Sets :=
     {|
-      fobj := λ G, obj[G];
+      fobj := λ G, obj[group G];
       fmap := λ G H ϕ, fobj ϕ;
     |}.
 
   Definition grp2mon (G : Object) : Mon.Object :=
     {|
-      Mon.obj := obj[G];
-      Mon.monoid := G;
+      Mon.obj := obj[group G];
+      Mon.monoid := group G;
     |}.
 End Grp.
-#[export] Coercion Grp.grp2mon : Grp.Object >-> Mon.Object.
+Coercion Grp.group : Grp.Object >-> Category.
+Coercion Grp.grp2mon : Grp.Object >-> Mon.Object.

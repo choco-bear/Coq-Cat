72a8c4ae02ae2308bad5b837d970d5eca0b0d627
choco-bear <kdy030529@gmail.com>
2026-02-21 19:14:18 +0900

    fix: add missing export for CategoryFacts in Common.v

--- theories/common/Common.v
+++ theories/common/Common.v
@@ -1,3 +1,3 @@
-Require Export CommonTactics CommonFacts Category Morphism.
+Require Export CommonTactics CommonFacts Category CategoryFacts Morphism.
 Require Export Functor FunctorTactics FunctorFacts.
 Require Export NatTrans NatTransTactics NatTransFacts.

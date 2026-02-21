799254cdaab8cd69a3b8adf4aafb6b1c6910ff02
choco-bear <kdy030529@gmail.com>
2026-02-21 13:50:12 +0900

    refactor: relfect the renaming and add existing instances

--- theories/categories/Concrete.v
+++ theories/categories/Concrete.v
@@ -18,12 +18,13 @@ Module Mon.
     |}%functor.
   Solve Obligations with functor_solver.
 
-  Program Definition Forgetful : Mon ⟶ Sets :=
+  Program Definition Forgetful : t ⟶ Sets :=
     {|
       fobj := λ M, obj[monoid M];
       fmap := λ M1 M2 ϕ, fobj ϕ;
     |}.
 End Mon.
+Existing Instance Mon.t.
 Coercion Mon.monoid : Mon.Object >-> Category.
 
 Module Grp.
@@ -43,7 +44,7 @@ Module Grp.
     |}%functor.
   Solve Obligations with functor_solver.
 
-  Program Definition Forgetful : Grp ⟶ Sets :=
+  Program Definition Forgetful : t ⟶ Sets :=
     {|
       fobj := λ G, obj[group G];
       fmap := λ G H ϕ, fobj ϕ;
@@ -55,5 +56,6 @@ Module Grp.
       Mon.monoid := group G;
     |}.
 End Grp.
+Existing Instance Grp.t.
 Coercion Grp.group : Grp.Object >-> Category.
 Coercion Grp.grp2mon : Grp.Object >-> Mon.Object.

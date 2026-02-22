0ef206d0f481487f3f2dc6e6844784e1fd9c1a01
choco-bear <kdy030529@gmail.com>
2026-02-22 17:01:35 +0900

    enhance: left and right composition tactics

--- theories/core/CategoryTactics.v
+++ theories/core/CategoryTactics.v
@@ -1,5 +1,11 @@
 Require Import CommonTactics CommonFacts Category.
 
+Create HintDb assoc_left discriminated.
+Global Hint Rewrite <- @comp_assoc : assoc_left.
+
+Create HintDb assoc_right discriminated.
+Global Hint Rewrite @comp_assoc : assoc_right.
+
 Lemma left_comp `{C : Category Obj} [x y z : Obj] (f g : x ~> y) (h : y ~> z)
   : f = g → h ∘ f =[C] h ∘ g.
 Proof. by intros ->. Qed.
@@ -9,35 +15,35 @@ Lemma right_comp `{C : Category Obj} [x y z : Obj] (f g : y ~> z) (h : x ~> y)
 Proof. by intros ->. Qed.
 
 Tactic Notation "left_comp" uconstr(p) :=
-  rewrite -?comp_assoc; unshelve simple refine (left_comp _ _ p _); rewrite ?comp_assoc.
+  autorewrite with assoc_left; unshelve simple refine (left_comp _ _ p _); autorewrite with assoc_right.
 
 Tactic Notation "left_comp" uconstr(p) "in" hyp(H) :=
   match type of H with
-  | @eq ?A ?f ?g => eapply (left_comp f g p) in H; rewrite ?comp_assoc in H
+  | @eq ?A ?f ?g => eapply (left_comp f g p) in H; autorewrite with assoc_right in H
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"
   | _ => fail 1 "The hypothesis" H "is not an equality"
   end.
   
 Tactic Notation "left_comp" uconstr(p) "in" hyp(H) "as" ident(name) :=
   match type of H with
-  | @eq ?A ?f ?g => eapply (left_comp f g p) in H as name; rewrite ?comp_assoc in name
+  | @eq ?A ?f ?g => eapply (left_comp f g p) in H as name; autorewrite with assoc_right in name
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"
   | _ => fail 1 "The hypothesis" H "is not an equality"
   end.
 
 Tactic Notation "right_comp" uconstr(p) :=
-  rewrite ?comp_assoc; unshelve simple refine (right_comp _ _ p _); rewrite ?comp_assoc.
+  autorewrite with assoc_right; unshelve simple refine (right_comp _ _ p _); autorewrite with assoc_right.
 
 Tactic Notation "right_comp" uconstr(p) "in" hyp(H) :=
   match type of H with
-  | @eq ?A ?f ?g => eapply (right_comp f g p) in H; rewrite ?comp_assoc in H
+  | @eq ?A ?f ?g => eapply (right_comp f g p) in H; autorewrite with assoc_right in H
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"
   | _ => fail 1 "The hypothesis" H "is not an equality"
   end.
 
 Tactic Notation "right_comp" uconstr(p) "in" hyp(H) "as" ident(name) :=
   match type of H with
-  | @eq ?A ?f ?g => eapply (right_comp f g p) in H as name; rewrite ?comp_assoc in name
+  | @eq ?A ?f ?g => eapply (right_comp f g p) in H as name; autorewrite with assoc_right in name
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"
   | _ => fail 1 "The hypothesis" H "is not an equality"
   end.

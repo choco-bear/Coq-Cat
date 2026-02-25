19c617c3770555af66ae36cc74d95b93202ce59d
choco-bear <kdy030529@gmail.com>
2026-02-26 06:09:15 +0900

    feat: add cancel tactics for morphism compositions

--- theories/core/MorphismTactics.v
+++ theories/core/MorphismTactics.v
@@ -16,5 +16,33 @@ Proof. i. comp_r h⁻¹ in H. common_simpl. Qed.
 Tactic Notation "comp_l" uconstr(p) :=
   autorewrite with assoc_right; unshelve simple refine (left_cancel _ _ p _); autorewrite with assoc_left.
 
+Tactic Notation "cancel_l" uconstr(p) "in" hyp(H) :=
+  match type of H with
+  | @eq ?A ?f ?g => unshelve eapply (left_cancel _ _ p) in H; autorewrite with assoc_left in H
+  | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms, or the term" p "is not appropriate"
+  | _ => fail 1 "The hypothesis" H "is not an equality"
+  end.
+
+Tactic Notation "cancel_l" uconstr(p) "in" hyp(H) "as" ident(name) :=
+  match type of H with
+  | @eq ?A ?f ?g => unshelve eapply (left_cancel _ _ p) in H as name; autorewrite with assoc_left in name
+  | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms, or the term" p "is not appropriate"
+  | _ => fail 1 "The hypothesis" H "is not an equality"
+  end.
+
 Tactic Notation "comp_r" uconstr(p) :=
   autorewrite with assoc_left; unshelve simple refine (right_cancel _ _ p _); autorewrite with assoc_left.
+
+Tactic Notation "cancel_r" uconstr(p) "in" hyp(H) :=
+  match type of H with
+  | @eq ?A ?f ?g => autorewrite with assoc_right in H; unshelve eapply (right_cancel _ _ p) in H; autorewrite with assoc_left in H
+  | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms, or the term" p "is not appropriate"
+  | _ => fail 1 "The hypothesis" H "is not an equality"
+  end.
+
+Tactic Notation "cancel_r" uconstr(p) "in" hyp(H) "as" ident(name) :=
+  match type of H with
+  | @eq ?A ?f ?g => autorewrite with assoc_right in H; unshelve eapply (right_cancel _ _ p) in H as name; autorewrite with assoc_left in H; autorewrite with assoc_left in name
+  | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms, or the term" p "is not appropriate"
+  | _ => fail 1 "The hypothesis" H "is not an equality"
+  end.

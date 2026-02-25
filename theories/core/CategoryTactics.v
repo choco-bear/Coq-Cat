35a36a27e50180ba4b73c5fdad9d6cc997e9edc3
choco-bear <kdy030529@gmail.com>
2026-02-25 17:03:15 +0900

    refactor: rename tactics manipulating morphisms to achieve more natural naming

--- theories/core/CategoryTactics.v
+++ theories/core/CategoryTactics.v
@@ -14,34 +14,34 @@ Lemma right_comp `{C : Category Obj} [x y z : Obj] (f g : y ~> z) (h : x ~> y)
   : f = g → f ∘ h =[C] g ∘ h.
 Proof. by intros ->. Qed.
 
-Tactic Notation "left_comp" uconstr(p) :=
+Tactic Notation "cancel_l" uconstr(p) :=
   autorewrite with assoc_left; unshelve simple refine (left_comp _ _ p _); autorewrite with assoc_right.
 
-Tactic Notation "left_comp" uconstr(p) "in" hyp(H) :=
+Tactic Notation "comp_l" uconstr(p) "in" hyp(H) :=
   match type of H with
   | @eq ?A ?f ?g => eapply (left_comp f g p) in H; autorewrite with assoc_right in H
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"
   | _ => fail 1 "The hypothesis" H "is not an equality"
   end.
   
-Tactic Notation "left_comp" uconstr(p) "in" hyp(H) "as" ident(name) :=
+Tactic Notation "comp_l" uconstr(p) "in" hyp(H) "as" ident(name) :=
   match type of H with
   | @eq ?A ?f ?g => eapply (left_comp f g p) in H as name; autorewrite with assoc_right in name
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"
   | _ => fail 1 "The hypothesis" H "is not an equality"
   end.
 
-Tactic Notation "right_comp" uconstr(p) :=
+Tactic Notation "cancel_r" uconstr(p) :=
   autorewrite with assoc_right; unshelve simple refine (right_comp _ _ p _); autorewrite with assoc_right.
 
-Tactic Notation "right_comp" uconstr(p) "in" hyp(H) :=
+Tactic Notation "comp_r" uconstr(p) "in" hyp(H) :=
   match type of H with
   | @eq ?A ?f ?g => eapply (right_comp f g p) in H; autorewrite with assoc_right in H
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"
   | _ => fail 1 "The hypothesis" H "is not an equality"
   end.
 
-Tactic Notation "right_comp" uconstr(p) "in" hyp(H) "as" ident(name) :=
+Tactic Notation "comp_r" uconstr(p) "in" hyp(H) "as" ident(name) :=
   match type of H with
   | @eq ?A ?f ?g => eapply (right_comp f g p) in H as name; autorewrite with assoc_right in name
   | @eq ?A ?f ?g => fail 1 "The hypothesis" H "is not an equality between morphisms"

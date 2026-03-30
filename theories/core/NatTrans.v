ac2f36905277e644db2ceb91e6b077d871547ac7
choco-bear <kdy030529@gmail.com>
2026-03-30 22:52:02 +0900

    refactor: update notation formatting for NatTrans and its components

--- theories/core/NatTrans.v
+++ theories/core/NatTrans.v
@@ -14,9 +14,9 @@ Arguments NatTrans {ObjC%_type_scope C%_category_scope ObjD%_type_scope D%_categ
 Arguments mk_NatTrans {ObjC%_type_scope C%_category_scope ObjD%_type_scope D%_category_scope} {F G}%_functor_scope (_ _)%_function_scope.
 Arguments component {ObjC%_type_scope C%_category_scope ObjD%_type_scope D%_category_scope} {F G}%_functor_scope x%_object_scope : rename, simpl never.
 
-Notation "F ⟹ G" := (NatTrans F G) (at level 70, no associativity) : type_scope.
+Notation "F ⟹ G" := (NatTrans F G) (at level 70, no associativity, format "F  ⟹  G") : type_scope.
 Notation "F '⟹@{' C ',' D '}' G" := (@NatTrans _ C%category _ D%category F%functor G%functor)
-  (at level 70, no associativity, format "F  ⟹@{ C , D }  G") : type_scope.
+  (at level 70, no associativity, only parsing) : type_scope.
 
 Program Definition IdNatTrans `{C : Category ObjC} `{D : Category ObjD} (F : C ⟶ D)
   : F ⟹ F := {| component := λ x, id[F x] |}%morphism.

96182e2822891b2ae28d0f8e690eebb2d21187d7
choco-bear <kdy030529@gmail.com>
2026-03-31 00:47:41 +0900

    refactor: manage the notations as a separate module

--- theories/categories/Sets.v
+++ theories/categories/Sets.v
@@ -16,17 +16,6 @@ Existing Instance Sets.t.
 Coercion Sets._set : Sets.Object >-> Sortclass.
 Coercion Sets.from_type : Sortclass >-> Sets.Object.
 
-Declare Scope sets_scope.
-Delimit Scope sets_scope with sets.
-Bind Scope sets_scope with Sets.Object.
-
-Notation "X × Y" := (Sets.from_type (Sets._set X%sets * Sets._set Y%sets)%type) : sets_scope.
-Notation "X + Y" := (Sets.from_type (Sets._set X%sets + Sets._set Y%sets)%type) : sets_scope.
-Notation "'2^' X" := (Sets.from_type (Sets._set X%sets → Prop)%type) (at level 8, right associativity, format "2^ X") : sets_scope.
-Notation "X → Y" := (Sets.from_type (Sets._set X%sets → Sets._set Y%sets)%type) : sets_scope.
-
-Local Open Scope sets_scope.
-
 Lemma Sets_id_unfold X x : id{Sets.t}[X]%morphism x = x.
 Proof. rewrite /cat_id //. Qed.
 Lemma Sets_comp_unfold [X Y Z : Sets.Object] (f : Y ~> Z) (g : X ~> Y) x : (f ∘ g)%morphism x = f (g x).
@@ -35,7 +24,7 @@ Proof. rewrite /comp //. Qed.
 
 Program Definition Powerset : Sets.t ⟶ Sets.t :=
   {|
-    fobj := λ X, 2^X;
+    fobj := λ X, X → Prop;
     fmap := λ X Y f P y, ∃ x, P x ∧ y = f x;
   |}.
 Next Obligation.
@@ -51,7 +40,7 @@ Qed.
 
 Program Definition BinaryProductSet : (Sets.t × Sets.t) ⟶ Sets.t :=
   {|
-    fobj := λ XY, XY.1 × XY.2;
+    fobj := λ XY, XY.1 * XY.2;
     fmap := λ XY1 XY2 fg xy, (fg.1 xy.1, fg.2 xy.2) 
   |}.
 Next Obligation.
@@ -59,4 +48,15 @@ Next Obligation.
   apply func_ext=> [] [x y] //.
 Qed.
 
-Notation "'(-×-)'" := BinaryProductSet (at level 0) : sets_scope.
+Module SetsNotations.
+  Declare Scope sets_scope.
+  Delimit Scope sets_scope with sets.
+  Bind Scope sets_scope with Sets.Object.
+  Open Scope sets_scope.
+
+  Notation "X × Y" := (Sets.from_type (Sets._set X%sets * Sets._set Y%sets)%type) : sets_scope.
+  Notation "X + Y" := (Sets.from_type (Sets._set X%sets + Sets._set Y%sets)%type) : sets_scope.
+  Notation "X → Y" := (Sets.from_type (Sets._set X%sets → Sets._set Y%sets)%type) : sets_scope.
+  Notation "'𝒫'" := Powerset (at level 0) : sets_scope.
+  Notation "'(-×-)'" := BinaryProductSet (at level 0) : sets_scope.
+End SetsNotations.

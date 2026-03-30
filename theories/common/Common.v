6a2a3a30c4d5d7c0560283fee48608b2b4a4cd96
choco-bear <kdy030529@gmail.com>
2026-03-30 23:28:13 +0900

    refactor: add `Global Set Primitive Projections` to `Common.v`

--- theories/common/Common.v
+++ theories/common/Common.v
@@ -3,3 +3,4 @@ Require Export Category CategoryTactics CategoryFacts.
 Require Export Morphism MorphismTactics MorphismFacts.
 Require Export Functor FunctorTactics FunctorFacts.
 Require Export NatTrans NatTransTactics NatTransFacts.
+Global Set Primitive Projections.

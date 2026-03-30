5ee49cc9c92e5999c5af68a08a27cb635c246fb6
choco-bear <kdy030529@gmail.com>
2026-03-31 06:55:20 +0900

    fix: remove unused NatTransFacts export from Common.v

--- theories/common/Common.v
+++ theories/common/Common.v
@@ -2,6 +2,6 @@ Require Export CommonTactics CommonFacts.
 Require Export Category CategoryTactics CategoryFacts.
 Require Export Morphism MorphismTactics MorphismFacts.
 Require Export Functor FunctorTactics FunctorFacts.
-Require Export NatTrans NatTransFacts.
+Require Export NatTrans.
 Global Set Primitive Projections.
 Global Close Scope nat_scope.

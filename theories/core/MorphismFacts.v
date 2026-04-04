14f2fc7f8beed092227beb765e2c92db3a44a6e8
choco-bear <kdy030529@gmail.com>
2026-04-04 22:10:07 +0900

    refactor: reorganize sections and improve structure in MorphismFacts.v

--- theories/core/MorphismFacts.v
+++ theories/core/MorphismFacts.v
@@ -1,24 +1,31 @@
-Require Import CommonTactics CommonFacts Category.
+Require Import CommonTactics CommonFacts Category CategoryTactics.
 Require Import Functor FunctorTactics FunctorFacts.
 Require Import Morphism MorphismTactics.
 
 Local Open Scope morphism_scope.
 
-#[export]
-Program Instance Functor_preserves_IsIso `{C : Category ObjC} `{D : Category ObjD} (T : C ⟶ D) `(f : x ~{C}~> y) `{!IsIsomorphism f}
-  : IsIsomorphism (T # f) := {| inverse_morphism := T # f⁻¹ |}.
-Next Obligation. rewrite -fmap_comp inv_morphism_left; common_simpl. Qed.
-Next Obligation. rewrite -fmap_comp inv_morphism_right; common_simpl. Qed.
+Section IsIsomorphism.
+  #[export]
+  Program Instance Functor_preserves_IsIso `{C : Category ObjC} `{D : Category ObjD} (T : C ⟶ D) `(f : x ~{C}~> y) `{!IsIsomorphism f}
+    : IsIsomorphism (T # f) := {| inverse_morphism := T # f⁻¹ |}.
+  Next Obligation. rewrite -fmap_comp inv_morphism_left; common_simpl. Qed.
+  Next Obligation. rewrite -fmap_comp inv_morphism_right; common_simpl. Qed.
+End IsIsomorphism.
 
-Lemma fmap_to_inv `{C : Category ObjC} `{D : Category ObjD} (T : C ⟶ D) `(f : x ~{C}~> y) `{!IsIsomorphism f}
-  : T # f⁻¹ = (T # f)⁻¹.
-Proof. common_simpl. Qed.
+Section FunctorSimpl.
+  Context `{C : Category ObjC} `{D : Category ObjD} (T : C ⟶ D).
 
+  Lemma fmap_to_inv `(f : x ~{C}~> y) `{!IsIsomorphism f}
+    : T # f⁻¹ = (T # f)⁻¹.
+  Proof. common_simpl. Qed.
+End FunctorSimpl.
 Hint Rewrite @fmap_to_inv : functor_prep.
 
-#[export]
-Instance BinaryProduct_preserves_IsGroupoid `[G : Category ObjG] `(!IsGroupoid G) `[H : Category ObjH] `(!IsGroupoid H) : IsGroupoid (G × H).
-Proof.
-  construct. depdes x y f.
-  construct; common_simpl.
-Qed.
+Section IsGroupoid.
+  #[export]
+  Instance BinaryProduct_preserves_IsGroupoid `[G : Category ObjG] `(!IsGroupoid G) `[H : Category ObjH] `(!IsGroupoid H) : IsGroupoid (G × H).
+  Proof.
+    construct. depdes x y f.
+    construct; common_simpl.
+  Qed.
+End IsGroupoid.

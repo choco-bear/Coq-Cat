09ea15cbba0143283e87d2588f50c9d50a397f0d
choco-bear <kdy030529@gmail.com>
2026-04-12 21:24:32 +0900

    refactor: update left_cancel and right_cancel lemmas to use Monic and Epic instead of IsIsomorphism

--- theories/core/MorphismTactics.v
+++ theories/core/MorphismTactics.v
@@ -5,13 +5,13 @@ Require Import Morphism.
 
 Local Open Scope morphism_scope.
 
-Lemma left_cancel `{C : Category Obj} [x y z : Obj] (f g : x ~> y) (h : y ~> z) `{!IsIsomorphism h}
+Lemma left_cancel `{C : Category Obj} [x y z : Obj] (f g : x ~> y) (h : y ~> z) `{!Monic h}
   : h ∘ f =[C] h ∘ g → f = g.
-Proof. i. comp_l h⁻¹ in H. common_simpl. Qed.
+Proof. common_simpl. Qed.
 
-Lemma right_cancel `{C : Category Obj} [x y z : Obj] (f g : y ~> z) (h : x ~> y) `{!IsIsomorphism h}
+Lemma right_cancel `{C : Category Obj} [x y z : Obj] (f g : y ~> z) (h : x ~> y) `{!Epic h}
   : f ∘ h =[C] g ∘ h → f = g.
-Proof. i. comp_r h⁻¹ in H. common_simpl. Qed.
+Proof. by apply epic. Qed.
 
 Tactic Notation "comp_l" uconstr(p) :=
   autorewrite with assoc_right; unshelve simple refine (left_cancel _ _ p _); autorewrite with assoc_left.

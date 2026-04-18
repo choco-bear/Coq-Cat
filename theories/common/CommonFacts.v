b4f9a019c51de97bcee768251fad8ce00da3a2e7
choco-bear <kdy030529@gmail.com>
2026-04-19 04:55:49 +0900

    refactor: reorganize inverse lemmas and improve context handling in Inverses section

--- theories/common/CommonFacts.v
+++ theories/common/CommonFacts.v
@@ -10,24 +10,23 @@ Arguments inverse_function [A B]%_type_scope f%_function_scope {BIJECTIVE} _ : r
   
 Notation "f '⁻¹'" := (inverse_function f) (at level 7, left associativity, format "f ⁻¹") : function_scope.
 
-Lemma inv_left_pointwise [A B : Type] (f : A → B) `{!Bijective f} x : f⁻¹ (f x) = x.
-Proof. transitivity ((f⁻¹ ∘ f) x)=> //. rewrite bijective_inv_left=> //. Qed.
+Section Inverses.
+  Context [A B : Type] (f : A → B) `{!Bijective f}.
 
-Lemma inv_right_pointwise [A B : Type] (f : A → B) `{!Bijective f} x : f (f⁻¹ x) = x.
-Proof. transitivity ((f ∘ f⁻¹) x)=> //. rewrite bijective_inv_right=> //. Qed.
+  Lemma inv_left_pointwise x : f⁻¹ (f x) = x.
+  Proof. transitivity ((f⁻¹ ∘ f) x)=> //. rewrite bijective_inv_left=> //. Qed.
 
-Lemma inv_spec [A B : Type] (f : A → B) `{!Bijective f} x y : y = f x → f⁻¹ y = x.
-Proof. intros ->. apply inv_left_pointwise. Qed.
+  Lemma inv_right_pointwise x : f (f⁻¹ x) = x.
+  Proof. transitivity ((f ∘ f⁻¹) x)=> //. rewrite bijective_inv_right=> //. Qed.
 
-Global Hint Rewrite @bijective_inv_left @bijective_inv_right @inv_left_pointwise @inv_right_pointwise @inv_spec : normalize.
+  Lemma inv_spec x y : y = f x → f⁻¹ y = x.
+  Proof. intros ->. apply inv_left_pointwise. Qed.
 
-Lemma inv_normalize_1 [A B C : Type] (f : A → B) `{!Bijective f} (g : B → C) : g ∘ f ∘ f⁻¹ = g.
-Proof. extensionalities=> /=. common_normalize=> //. Qed.
+  Lemma inv_normalize_1 [C : Type] (g : B → C) : g ∘ f ∘ f⁻¹ = g.
+  Proof. extensionalities=> /=. rewrite inv_right_pointwise //. Qed.
 
-Lemma inv_normalize_2 [A B C : Type] (f : A → B) `{!Bijective f} (g : A → C) : g ∘ f⁻¹ ∘ f = g.
-Proof. extensionalities=> /=. common_normalize=> //. Qed.
-
-Global Hint Rewrite @inv_normalize_1 @inv_normalize_2 : normalize.
-
-Global Instance prop_pi (P : Prop) : ProofIrrel P.
-Proof. ii. apply proof_irr. Qed.
+  Lemma inv_normalize_2 [C : Type] (g : A → C) : g ∘ f⁻¹ ∘ f = g.
+  Proof. extensionalities=> /=. rewrite inv_left_pointwise //. Qed.
+End Inverses.
+Global Hint Rewrite @bijective_inv_left @bijective_inv_right @inv_left_pointwise @inv_right_pointwise
+                    @inv_spec @inv_normalize_1 @inv_normalize_2 : normalize.
